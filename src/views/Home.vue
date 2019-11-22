9e57bb9d36562c0061a9b096b6dbca4034111934
Charles Roh <chuckyroh@gmail.com>
2019-11-21 21:22:13 -0800

    add purgecss and hero banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
 	<div id="home">
-		
+		<div class="container">
+			<div class="h-screen w-screen flex justify-center items-center bg-blue-100">
+				<span class="text-6xl">CHARLES</span>
+				<span class="text-6xl">ROH</span>
+			</div>
+		</div>
 	</div>
 </template>
 
